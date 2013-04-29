c7e24133bc74f8d672624fda57433185dfc26b25
Oleg Seletsky <oleg.b.seletsky@gmail.com>
2013-04-29 10:24:44 -0700

    adding example app bundler_sample_1

---
+++ examples/bundler_sample_1/Gruntfile.js
@@ -0,0 +1,35 @@
+//var _ = require( "underscore" ),
+//	_s = require( "underscore.string" ),
+//	path = require( "path" ),
+	//assetManager = require( "./WebServer/Libraries/assetManager.js" );
+//	assetManager = require( "assetManager.js" );
+
+module.exports = function( grunt ) {
+
+	grunt.initConfig( {
+
+		pkg: grunt.file.readJSON( "package.json" ),
+
+		bundler : {
+			dist : {
+				options : {
+					mode : "prod",
+					assetLibrarySrc : "AssetLibrary/",
+					assetLibraryDest : "WebServer/Static/AssetLibrary-assets/",
+					appPagesSrc : "WebServer/AppPages/",
+					appPagesDest : "WebServer/Static/AppPages-assets/"
+				}
+			}
+		}
+	} );
+
+	grunt.loadNpmTasks( "grunt-contrib-copy" );
+	grunt.loadNpmTasks( "grunt-contrib-clean" );
+	grunt.loadNpmTasks( "grunt-contrib-sass");
+	grunt.loadNpmTasks( "grunt-contrib-compass");
+	grunt.loadNpmTasks( "grunt-contrib-concat" );
+	grunt.loadNpmTasks( "grunt-contrib-watch" );
+	grunt.loadNpmTasks( "grunt-bundler" );
+
+
+};
---
+++ examples/bundler_sample_1/WebServer/app.js
@@ -0,0 +1,65 @@
+
+/**
+* Module dependencies.
+*/
+
+var express = require( "express" ),
+	//routeManager = require( "./Libraries/routeManager.js" ),
+	assetManager = require( "grunt-bundler" ).assetManager,
+	http = require( "http" ),
+	cons = require('consolidate'),
+	swig = require('swig'),
+	path = require( "path" ),
+	fs = require( "fs" ),
+	_ = require( "underscore" ),
+	_s = require( "underscore.string" );
+
+
+var app = express();
+
+var projectRoot = "../";
+
+app.configure( function() {
+	app.set( "port" , process.env.PORT || 3000 );
+	app.set( "views" , __dirname + "/AppPages" );
+	//app.set( "views", __dirname );
+	app.set( "view engine", "swig");
+	app.use( express.favicon() );
+	app.use( express.logger( "dev" ) );
+	app.use( express.bodyParser() );
+	app.use( express.methodOverride() );
+	//app.use( myMethod() );
+	app.use( assetManager.bundlerMiddleware( path.join( __dirname, projectRoot ) ) );
+	app.use( express.cookieParser( "your secret here" ) );
+	app.use( express.session() );
+	app.use( app.router );
+	//app.use( express.static( path.join( __dirname, "Static" ) ) );
+	app.use( express.static( path.join( __dirname, projectRoot ) ) );
+} );
+
+app.engine( ".swig", cons.swig );
+
+swig.init({
+    allowErrors: true, // allows errors to be thrown and caught by express instead of suppressed by Swig
+    root : __dirname + "/AppPages"
+});
+
+app.configure( "development" , function() {
+	app.use( express.errorHandler() );
+} );
+
+app.get( "/page1/page1.html.swig", function( req, res ) {
+	res.render( "page1/page1.html.swig" );
+} );
+
+app.get("/page1/page1_1/page1_1.html.swig", function( req, res ) {
+	res.render( "/page1/page1_1/page1_1.html.swig" );
+} );
+
+app.get( "/login*", function( req, res ) {
+	res.render( "web-terminal/login.swig" );
+} );
+
+http.createServer( app ).listen( app.get( "port" ), function() {
+	console.log( "Express server listening on port " + app.get("port") );
+} );
