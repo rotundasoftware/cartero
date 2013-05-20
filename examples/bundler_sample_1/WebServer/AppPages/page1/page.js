3c199d0cd9136ed60e00e03fa48633b6fef24e8e
Oleg Seletsky <oleg.b.seletsky@gmail.com>
2013-05-20 09:21:38 -0700

    throw error if file listed in filePriority in bundle.json does not exist.  add support for prioritizeSubdirectories in bundle.json - defaults to false.

--- examples/bundler_sample_1/WebServer/AppPages/page1/page.js
+++ examples/bundler_sample_1/WebServer/AppPages/page1/page.js
@@ -2,4 +2,6 @@ $(document).ready( function() {
 
 	$( "#tabs" ).tabs();
 
+	console.log( "this should not be 2 bundler_dir: #bundler_dir" );
+
 } );
--- examples/bundler_sample_2/App/WebServer/AppPages/page1/page1.js
+++ examples/bundler_sample_2/App/WebServer/AppPages/page1/page1.js
@@ -1,6 +1,4 @@
 
 var lib3 = require( "Lib1/Lib3/lib3.js" );
 
-console.log( "me ASDF" + lib3.identify() );
-
-//console.log( "meeeeeee ASDF" );
+console.log( "me ASDF" + lib3.identify() + " dd #bundler_dir" );
