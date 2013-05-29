b3f656633955fe2bb99bbe3bb4c5f06f9716750f
Oleg Seletsky <oleg.b.seletsky@gmail.com>
2013-05-28 18:22:18 -0700

    add support for postProcessor and change format of pageMap.json, move script/link/tmpl src building to middleware

--- middleware.js
+++ middleware.js
@@ -1,11 +1,18 @@
-var assetBundlerUtil = require( "./lib/util.js" );
+var assetBundlerUtil = require( "./lib/util.js" ),
+	fs = require( "fs" ),
+	_ = require( "underscore" ),
+	path = require( "path" );
 
 module.exports = function( rootDir, staticDir, appPagesDir ) {
 
 	var pageMap;
+	var configMap;
+	var mode;
 
 	try {
 		pageMap = assetBundlerUtil.readPageMap();
+		configMap = assetBundlerUtil.readBundlerConfig();
+		mode = configMap.mode;
 	}
 	catch( e ) {
 		throw new Error( "Error while reading pageMap file. Please run the grunt assetbundler task before running your application." );
@@ -15,15 +22,28 @@ module.exports = function( rootDir, staticDir, appPagesDir ) {
 
 		var oldRender = res.render;
 
-		res.render = function( path, options ) {
-			var pageMapKey = options && options.bundler_pageMapKey ? options.bundler_pageMapKey : path.replace( appPagesDir, "" ).substring( 1 );
+		res.render = function( requestPath, options ) {
+			var pageMapKey = options && options.bundler_pageMapKey ? options.bundler_pageMapKey : requestPath.replace( appPagesDir, "" ).substring( 1 );
 			console.log( pageMapKey );
 			
-			var pageMetadata = pageMap[ pageMapKey ];
+			var pageMetadata = pageMap[ pageMapKey ][ mode ];
 
-			res.locals.bundler_js = pageMetadata.bundler_js;
-			res.locals.bundler_css = pageMetadata.bundler_css;
-			res.locals.bundler_tmpl = pageMetadata.bundler_tmpl;
+			res.locals.bundler_js = _.map( pageMetadata.js, function( fileName ) {
+				return "<script type='text/javascript' src='/" + fileName + "'></script>";
+			} ).join( "" );
+
+			res.locals.bundler_css = _.map( pageMetadata.css, function( fileName ) {
+				return "<link rel='stylesheet' href='/" + fileName + "'></link>";
+			} ).join( "" );
+
+			if( mode === "dev" ) {
+				res.locals.bundler_tmpl = _.map( pageMetadata.tmpl, function( fileName ) {
+					return fs.readFileSync( staticDir + path.sep + fileName ).toString();
+				} ).join( "" );
+			}
+			else {
+				res.locals.bundler_tmpl = pageMetadata.tmpl_cache.join( "" );
+			}
 
 			oldRender.apply( res, arguments );
 		};
