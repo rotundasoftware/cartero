190bcef6179f9a2fceb8331abae7856a9ad54b12
Oleg Seletsky <oleg.b.seletsky@gmail.com>
2014-02-25 11:43:16 -0800

    work on test example for new cartero

---
+++ test/example1/views/page1/page1.js
@@ -0,0 +1,8 @@
+var _ = require( "underscore" );
+var customLibrary1 = require( "customLibrary1" );
+
+if( _ !== undefined ) {
+	console.log( "we got underscore!" );
+}
+
+console.log( "customLibrary1: " + customLibrary1 );
