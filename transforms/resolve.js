440a312b4c46f8ea88fb3f20a01207b150552699
David Beck <david@rotundasoftware.com>
2016-11-22 15:30:34 -0800

    resolve issue with resolve transform and symlinked modules

--- transforms/resolve.js
+++ transforms/resolve.js
@@ -22,13 +22,50 @@ module.exports = function( file, options ) {
 				return _this.emit( 'error', new Error( 'Could not resolve ##resolve( "' + unresolvedPath + '" ) in file "' + file + '": ' + err ) );
 			}
 
-			resolvedPath = fs.realpathSync( resolvedPath );
+			var resolvedRealPath = fs.realpathSync( resolvedPath );
+		
+			if( ! options.appRootDir ) return resolvedRealPath;
 
-			if( options.appRootDir ) {
-				resolvedPath = path.relative( options.appRootDir, resolvedPath );
+			var relativePath = path.relative( options.appRootDir, resolvedRealPath );
+
+			var relativePathItems = relativePath.split( '/' );
+			if( relativePathItems[ 0 ] === '..' ) {
+				// uff, the file we are resolving is OUTSIDE of the root dir of the
+				// application. This happens when a symlinked module (i.e. using npm link)
+				// has a ##resolve transform that references a file in another symlinked
+				// module. The 'file' argument that browserify passes to this transform
+				// method is a real path, but we want it to be the symlinked path, so that
+				// all our "resolves" give us paths inside the app root dir. Clearly,
+				// reconstructing the symlinked path given the real path is not straight
+				// forward. I'm not even sure why the below logic works, but it does.
+				// Will leave it alone for now!
+
+				// try again using symlinked path
+				relativePath = path.relative( options.appRootDir, resolvedPath );
+				relativePathItems = relativePath.split( '/' );
+
+				if( relativePathItems[ 0 ] === '..' ) {
+					// still didn't work. now we presume that at some point last part of the
+					// symlinked path is going to mirror what we have relative to the app root dir.
+					// this generally works because after the node_modules part, they are both the same.
+					// so we keep chopping off the start of the symlinked path until we find a file
+					// that is in that location relative to the app root dir. again, not 100% sure
+					// this works for all cases, but seems to work for ours.
+
+					do {
+						relativePathItems.shift();
+					} while(
+						relativePathItems.length &&
+						! fs.existsSync( path.join( options.appRootDir, relativePathItems.join( '/' ) ) )
+					)
+
+					if( relativePathItems.length ) {
+						relativePath = relativePathItems.join( '/' );
+					}
+				}
 			}
 
-			return resolvedPath;
+			return relativePath;
 		} );
 
 		this.queue( new Buffer( res, 'utf8' ) );
